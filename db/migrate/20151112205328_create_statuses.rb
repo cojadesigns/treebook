22329d00c9a703b9e3298f55088b106178b42208
Akitoye Joseph <cojadesigns@gmail.com>
2015-11-14 17:39:41 +0000

    Add bootstrap, and applied layout to the page

--- db/migrate/20151112205328_create_statuses.rb
+++ db/migrate/20151112205328_create_statuses.rb
@@ -1,10 +1,14 @@
 class CreateStatuses < ActiveRecord::Migration
-  def change
+  def up
     create_table :statuses do |t|
       t.string :name
       t.text :content
 
       t.timestamps null: false
     end
+  end
+
+  def down
+    drop_table :statuses
   end
 end
